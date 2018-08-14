27cd4a8a2fcf9959e11fdbaaa8993a9488727d1b
Halimao <1065621723@qq.com>
2018-08-14 11:06:28 +0800

    添加异常捕捉，防止异常退出主程序

--- main.go
+++ main.go
@@ -6,13 +6,32 @@ import (
 	"net/http"
 )
 
+// tryCatch 实现 try catch.
+// 空接口：具有0个方法的接口称为空接口。它表示为interface {}。由于空接口有0个方法，所有类型都实现了空接口
+func tryCatch(try func(phone, passwd string), handler func(interface{}), phone string, passwd string) {
+	defer func() {
+		if err := recover(); err != nil {
+			handler(err)
+		}
+	}()
+	try(phone, passwd)
+}
+
 func main() {
-	defer util.Pause()
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Printf("%v", err)
+			util.Pause()
+		}
+	}()
 	authArray, port := util.InitConfig()
 	if len(authArray) > 0 {
 		for _, value := range authArray {
 			if value.Valid {
-				go util.DoTask(value.Phone, value.Passwd)
+				// go util.DoTask(value.Phone, value.Passwd)
+				go tryCatch(util.DoTask, func(err interface{}) {
+					fmt.Printf("phone:%s, Open treasure box failed, will be removed from account storage. %v\n", value.Phone, err)
+				}, value.Phone, value.Passwd)
 			}
 		}
 	}
