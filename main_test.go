79ca15814efee3a95e99ed5e94af8c498427f334
Halimao <1065621723@qq.com>
2018-08-17 11:25:20 +0800

    旧日志改为压缩

--- main_test.go
+++ main_test.go
@@ -6,17 +6,24 @@ import (
 	"testing"
 	"time"
 
+	log "github.com/cihub/seelog"
 	"github.com/robfig/cron"
 )
 
 // Login 模拟登录
 func TestAny(t *testing.T) {
+	logger, err := log.LoggerFromConfigAsFile("conf/seelog.xml")
+	if err != nil {
+		fmt.Println(err)
+		panic("Open seelog.xml failed!")
+	}
 	fmt.Println("current time: ", time.Now().Format("2006-01-02 15:04:05"))
 	c := cron.New()
 	//spec := "5 */1 * * * ?"
-	spec := "@every 1m3s"
+	spec := "@every 3s"
 	c.AddFunc(spec, func() {
 		fmt.Println("cron running: ", time.Now().Format("2006-01-02 15:04:05"))
+		logger.Info("cron running: ", time.Now().Format("2006-01-02 15:04:05"))
 	})
 	c.Start()
 	// now := time.Now()
